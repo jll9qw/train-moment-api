4983c7defc015fdc0811742fdbe05dbadf4a1474
jll9qw <lloyd804@gmail.com>
2019-03-30 20:39:39 -0400

    final

--- assets/app.js
+++ assets/app.js
@@ -14,7 +14,7 @@ $(document).ready(function() {
   var trainName = "";
   var destination = "";
   var trainTime = "";
-  var trainFreq = 0;
+  var trainFreq = "";
 
   $("#submit-button").on("click", function(event) {
     event.preventDefault();
@@ -38,110 +38,69 @@ $(document).ready(function() {
       trainTime: trainTime,
       trainFreq: trainFreq,
       dateAdded: firebase.database.ServerValue.TIMESTAMP
-
-     
     });
 
     resetFields();
+  });
+  // reset function
+  function resetFields() {
+    $("#train-name-input").val("");
+    $("#destination-input").val("");
+    $("#first-time-input").val("");
+    $("#frequency-input").val("");
+  }
+
+  dataRef.ref().on(
+    "child_added",
+    function(snapshot) {
+      var nextTrain = calculateTimes(
+        snapshot.val().trainTime,
+        snapshot.val().trainFreq
+      );
+
+      console.log(snapshot.val().trainName);
+      console.log(snapshot.val().destination);
+      console.log(snapshot.val().trainFreq);
+
+      // Output to HTML
+      $("#train-schedule").append(
+        "<tr><th> " +
+          snapshot.val().trainName +
+          " </th><td> " +
+          snapshot.val().destination +
+          " </td><td> " +
+          snapshot.val().trainTime +
+          " </td><td> " +
+          snapshot.val().trainFreq +
+          " </td><td>  " +
+          nextTrain +
+          "</td></tr>"
+      );
+    },
+
+    // Handle the errors
+    function(errorObject) {
+      console.log("Errors handled: " + errorObject.code);
+    }
+  );
 });
 
-    function resetFields() {
-        $("#train-name-input").val("");
-        $("#destination-input").val("");
-        $("#first-time-input").val("");
-        $("#frequency-input").val("");
-      }
+function calculateTimes(firstTrain, trainFreq) {
+  var currentTime = moment();
+  var stationStart = moment(firstTrain, "HH:mm");
 
+  var minutesTilNextTrain = 0;
 
+  if (stationStart > currentTime) {
+    minutesTilNextTrain = stationStart.diff(currentTime, "minutes");
+  } else {
+    // Otherwise, get minutes past first time
+    var minutesPast = currentTime.diff(stationStart, "minutes");
+    // Find the result of minutesPast % trainFreq
+    var remainder = minutesPast % trainFreq;
+    // Subtract the remainder from the trainFreq
+    minutesTilNextTrain = trainFreq - remainder;
+  }
 
-      function calculateTimes(trainFreq, trainTime) {
-        var tfreq = trainFreq;
-
-        var firstTime = trainTime;
-
-        var firstTimeConverted = moment(firstTime, "H HH").subtract(
-          1,
-          "years"
-        );
-        console.log(firstTimeConverted);
-
-        var currentTime = moment();
-        console.log("Current Time: " + moment(currentTime).format("H HH"));
-
-        var diffTime = moment().diff(moment(firstTimeConverted), "m mm");
-        console.log("DIFFERENCE IN TIME: " + diffTime);
-
- 
-        var tRemainder = diffTime % tfreq;
-        console.log(tRemainder);
-
-   
-        var tMinutesTillTrain = tfreq - tRemainder;
-        console.log("MINUTES TILL TRAIN: " + tMinutesTillTrain);
- 
-
-        var nextTrain = moment().add(tMinutesTillTrain, "m mm");
-        console.log("ARRIVAL TIME: " + moment(nextTrain).format("H HH"));
-
-        return {
-          arrival: moment(nextTrain).format("H HH"),
-          minutesAway: tMinutesTillTrain
-        };
-
-        // if (firstTime > currentTime) {
-        //     nextTrain = firstTime;
-        //   } else {
-        //     // Otherwise, get minutes past first time
-        //     var minutesPast = currentTime.diff(firstTime, 'minutes');
-        //     // Find the result of minutesPast % frequency
-        //     var remainder = minutesPast % frequency;
-        //     // Subtract the remainder from the frequency
-        //     var minutesTilNextTrain = frequency - remainder;
-        //     // Set nextTrain to the currentTime + `minutesTilNextTrain` minutes
-        //     nextTrain = currentTime.add(minutesTilNextTrain, 'minutes');
-        //   }
-      
-      
-      }
-
-      dataRef.ref().on(
-        "child_added",
-        function(snapshot) {
-          console.log(snapshot.val().name);
-          console.log(snapshot.val().destination);
-          console.log(snapshot.val().frequency);
-
-          var times = calculateTimes(
-            snapshot.val().frequency,
-            snapshot.val().time
-          );
-
-          // Output to HTML
-          $("#results-table").append(
-            "<tr><td> " +
-              snapshot.val().trainName +
-              " </td><td> " +
-              snapshot.val().destination +
-              " </td><td> " +
-              snapshot.val().trainTime +
-              " </td><td> " +
-              snapshot.val().trainFreq +
-              " </td><td> " +
-              times.arrival +
-              "</td><td>  " +
-              times.minutesAway +
-              "</td></tr>"
-          );
-
-        },
-
-
-          // Handle the errors
-        function(errorObject) {
-          console.log("Errors handled: " + errorObject.code);
-        }
-)});
-
-
-
-    
+  return minutesTilNextTrain;
+}
